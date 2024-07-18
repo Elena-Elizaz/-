3f9cf1a1705d8443e804f2b1c8dd654ceadb3f84
Elena-Elizaz <opryatova96@mail.ru>
2024-07-18 20:55:42 +1000

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,14 @@
+homework= 12
+hours_spent= 1.5
+Course_name= 'Python'
+Time_to_task= (hours_spent/homework)
+print("Курс:",Course_name, "всего задач:",homework, "затрачено часов:",hours_spent, "Среднее время выполнения:",Time_to_task, "часа." )
+
+
+
+
+
+
+
+
+
